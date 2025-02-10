22b6d480745ab8d9d4ca6ebd6ae515bc04617ada
Glenn Harvey Liwanag <glennharveyliwanag@gmail.com>
2025-02-11 00:15:32 +0800

    Customize init

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,107 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+-- For gopls, we need to use `textDocument/codeAction` to organize imports
+-- https://github.com/neovim/nvim-lspconfig/issues/115
+local golang_organize_imports = function(bufnr, isPreflight)
+  local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(bufnr))
+  params.context = { only = { 'source.organizeImports' } }
+
+  if isPreflight then
+    vim.lsp.buf_request(bufnr, 'textDocument/codeAction', params, function() end)
+    return
+  end
+
+  local result = vim.lsp.buf_request_sync(bufnr, 'textDocument/codeAction', params, 3000)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(bufnr))
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('LspFormatting', {}),
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client.name == 'gopls' then
+      -- hack: Preflight async request to gopls, which can prevent blocking when save buffer on first time opened
+      golang_organize_imports(bufnr, true)
+
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.go',
+        group = vim.api.nvim_create_augroup('LspGolangOrganizeImports.' .. bufnr, {}),
+        callback = function()
+          golang_organize_imports(bufnr)
+        end,
+      })
+    end
+  end,
+})
+
+return {
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter', -- Lazy load when entering insert mode
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true, -- Automatically trigger suggestions
+          debounce = 75, -- Debounce time in ms
+          keymap = {
+            accept = '<C-J>', -- Keybinding to accept a suggestion
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        panel = {
+          enabled = true,
+          layout = {
+            position = 'bottom', -- Display panel at the bottom
+            ratio = 0.4, -- Height ratio
+          },
+        },
+        filetypes = {
+          go = true,
+          python = true,
+          javascript = true,
+          typescript = true,
+          lua = true,
+          yaml = true,
+          markdown = true,
+          help = false,
+          gitcommit = true,
+          gitrebase = true,
+          hgcommit = true,
+          ['*'] = false, -- Disable by default for all filetypes
+        },
+      }
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
