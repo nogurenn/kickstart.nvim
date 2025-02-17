0c6f2bb5d50383644f33e15b5b005b09cbd2257b
Glenn Harvey Liwanag <glennharveyliwanag@gmail.com>
2025-02-18 01:38:52 +0800

    Personalize

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -410,13 +410,23 @@ require('lazy').setup({
         --   },
         -- },
         defaults = {
+          layout_strategy = 'flex',
           file_ignore_patterns = {
             'node_modules',
+            '.git',
+            'mocks',
           },
         },
         pickers = {
           find_files = {
             hidden = true,
+            no_ignore = true,
+            -- follow = true,
+          },
+        },
+        previewers = {
+          vimgrep = {
+            split = true,
           },
         },
         extensions = {
@@ -679,7 +689,19 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                shadow = true,
+              },
+              staticcheck = true,
+              completeUnimported = true,
+              usePlaceholders = true,
+            },
+          },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,27 +3,44 @@
 --
 -- See the kickstart.nvim README for more information
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.softtabstop = 4
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page and center cursor' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page and center cursor' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Move to next search result and center cursor' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Move to previous search result and center cursor' })
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Paste and return to that same register/yank' })
+-- Move highlighted line / block of text in visual mode
+vim.keymap.set('x', 'J', ":move '>+1<CR>gv=gv", { desc = 'Move highlighted block down' })
+vim.keymap.set('x', 'K', ":move '<-2<CR>gv=gv", { desc = 'Move highlighted block up' })
+-- Append line below without moving cursor
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Append line below without moving cursor' })
+
+require('custom.plugins.telescope.multigrep').setup()
+
 -- For gopls, we need to use `textDocument/codeAction` to organize imports
 -- https://github.com/neovim/nvim-lspconfig/issues/115
 local golang_organize_imports = function(bufnr, isPreflight)
   local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding(bufnr))
   params.context = { only = { 'source.organizeImports' } }
 
-  if isPreflight then
-    vim.lsp.buf_request(bufnr, 'textDocument/codeAction', params, function() end)
-    return
-  end
-
-  local result = vim.lsp.buf_request_sync(bufnr, 'textDocument/codeAction', params, 3000)
-  for _, res in pairs(result or {}) do
+  -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+  -- machine and codebase, you may want longer. Add an additional
+  -- argument after params if you find that you have to write the file
+  -- twice for changes to be saved.
+  -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+  for cid, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding(bufnr))
-      else
-        vim.lsp.buf.execute_command(r.command)
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
       end
     end
   end
+  vim.lsp.buf.format { async = false }
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -64,6 +81,8 @@ return {
           debounce = 75, -- Debounce time in ms
           keymap = {
             accept = '<C-J>', -- Keybinding to accept a suggestion
+            accept_line = '<M-l>', -- Keybinding to accept a whole line
+            accept_word = '<M-j>', -- Keybinding to accept a whole word
             next = '<M-]>',
             prev = '<M-[>',
             dismiss = '<C-]>',
